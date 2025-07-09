b76e1f3de51a8afdc4e1c3af21605667b7b58814
黄海志 <1839462565@qq.com>
2025-07-10 00:06:24 +0800

    1.redis实现短信登录 2.用户脱敏 3.完成两个拦截器,一个是登录拦截,一个是更新拦截

--- hm-dianping/src/main/java/com/hmdp/config/MybatisConfig.java
+++ hm-dianping/src/main/java/com/hmdp/config/MybatisConfig.java
@@ -3,8 +3,14 @@ package com.hmdp.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.hmdp.utils.LoginInterceptor;
+import com.hmdp.utils.RefreshTokenInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+
+import javax.annotation.Resource;
 
 @Configuration
 public class MybatisConfig {
